01df6ab118a3ca462cf0f3d7dad521bab7eb5a9d
Sanchit Verma <sanchit.verma.167@gmail.com>
2021-03-03 18:22:20 +0530

    minor change to main file text change

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             // Text("Platform",style: TextStyle(color: Platform.isAndroid?Colors.red:Colors.blue),),
             Text(
-              "Verma",
+              "Flutter upgrade to new",
               style: TextStyle(color: kIsWeb ? Colors.red : Colors.yellow),
             )
           ],
